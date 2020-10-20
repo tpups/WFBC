d5134e29970873a442a0e2b7c4d7def74bfb663f
tpups <jghtch@gmail.com>
2020-10-20 00:36:31 -0700

    styling draft picks sheet

---
+++ wfbc.page/Client/Pages/DraftPicks.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wfbc.page.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+
+namespace wfbc.page.Client.Pages
+{
+    public class DraftPicksDataModel : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetPicks();
+        }
+        protected async Task GetPicks()
+        {
+
+        }
+    }
+}
