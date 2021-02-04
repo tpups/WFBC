d4b5279d0ad0c2743bc62c223aba6d9b6ea0f193
tpups <jghtch@gmail.com>
2021-02-03 22:13:32 -0800

    Update to .NET 5.0

--- wfbc.page/Client/Program.cs
+++ wfbc.page/Client/Program.cs
@@ -17,7 +17,7 @@ namespace WFBC.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddOidcAuthentication(options =>
             {
@@ -41,7 +41,7 @@ namespace WFBC.Client
 
             builder.Services.AddSingleton<AppState>();
 
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
