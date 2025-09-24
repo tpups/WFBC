b248882ada4997de9750699cd0b0b0b7217130d7
Josh Hutchinson <jghtch@gmail.com>
2025-09-23 19:13:29 -0700

    programattic db year range

--- wfbc.page/Server/Models/AppSettings.cs
+++ wfbc.page/Server/Models/AppSettings.cs
@@ -4,12 +4,16 @@
     {
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
+        int StartYear { get; set; }
+        int EndYear { get; set; }
     }
 
     public class DatabaseSettings : IDatabaseSettings
     {
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
     }
 
     public interface IOktaSettings
--- wfbc.page/Server/Models/WfbcDBContext.cs
+++ wfbc.page/Server/Models/WfbcDBContext.cs
@@ -8,54 +8,22 @@ namespace WFBC.Server.Models
     {
         private readonly IMongoDatabase _wfbcDatabase;
         private readonly Dictionary<string, IMongoDatabase> _dbs;
-        private readonly IMongoDatabase _wfbc2011;
-        private readonly IMongoDatabase _wfbc2012;
-        private readonly IMongoDatabase _wfbc2013;
-        private readonly IMongoDatabase _wfbc2014;
-        private readonly IMongoDatabase _wfbc2015;
-        private readonly IMongoDatabase _wfbc2016;
-        private readonly IMongoDatabase _wfbc2017;
-        private readonly IMongoDatabase _wfbc2018;
-        private readonly IMongoDatabase _wfbc2019;
-        private readonly IMongoDatabase _wfbc2020;
-        private readonly IMongoDatabase _wfbc2021;
-        private readonly IMongoDatabase _wfbc2022;
-        private readonly IMongoDatabase _wfbc2023;
-
         private readonly MongoClient client;
+        private readonly IDatabaseSettings _settings;
         public WfbcDBContext(IDatabaseSettings settings)
         {
+            _settings = settings;
             _dbs = new Dictionary<string, IMongoDatabase>();
             client = new MongoClient(settings.ConnectionString);
             _wfbcDatabase = client.GetDatabase(settings.DatabaseName);
 
-            _wfbc2011 = client.GetDatabase("wfbc2011");
-            _dbs.Add("2011", _wfbc2011);
-            _wfbc2012 = client.GetDatabase("wfbc2012");
-            _dbs.Add("2012", _wfbc2012);
-            _wfbc2013 = client.GetDatabase("wfbc2013");
-            _dbs.Add("2013", _wfbc2013);
-            _wfbc2014 = client.GetDatabase("wfbc2014");
-            _dbs.Add("2014", _wfbc2014);
-            _wfbc2015 = client.GetDatabase("wfbc2015");
-            _dbs.Add("2015", _wfbc2015);
-            _wfbc2016 = client.GetDatabase("wfbc2016");
-            _dbs.Add("2016", _wfbc2016);
-            _wfbc2017 = client.GetDatabase("wfbc2017");
-            _dbs.Add("2017", _wfbc2017);
-            _wfbc2018 = client.GetDatabase("wfbc2018");
-            _dbs.Add("2018", _wfbc2018);
-            _wfbc2019 = client.GetDatabase("wfbc2019");
-            _dbs.Add("2019", _wfbc2019);
-            _wfbc2020 = client.GetDatabase("wfbc2020");
-            _dbs.Add("2020", _wfbc2020);
-            _wfbc2021 = client.GetDatabase("wfbc2021");
-            _dbs.Add("2021", _wfbc2021);
-            _wfbc2022 = client.GetDatabase("wfbc2022");
-            _dbs.Add("2022", _wfbc2022);
-            _wfbc2023 = client.GetDatabase("wfbc2023");
-            _dbs.Add("2023", _wfbc2023);
-
+            // Dynamic year database initialization
+            for (int year = settings.StartYear; year <= settings.EndYear; year++)
+            {
+                string yearStr = year.ToString();
+                var yearDb = client.GetDatabase($"wfbc{yearStr}");
+                _dbs.Add(yearStr, yearDb);
+            }
         }
 
         public IMongoCollection<Draft> Drafts
@@ -91,7 +59,7 @@ namespace WFBC.Server.Models
             get
             {
                 Dictionary<string, IMongoCollection<Standings>> _standings = new Dictionary<string, IMongoCollection<Standings>>();
-                for (int i = 2011;  i <= 2023; i++)
+                for (int i = _settings.StartYear; i <= _settings.EndYear; i++)
                 {
                     string year = i.ToString();
                     _standings.Add(year, _dbs[year].GetCollection<Standings>("standings"));
@@ -104,7 +72,7 @@ namespace WFBC.Server.Models
             get
             {
                 Dictionary<string, Dictionary<string, IMongoCollection<Box>>> _boxScores = new Dictionary<string, Dictionary<string, IMongoCollection<Box>>>();
-                for (int i = 2011; i <= 2023; i++)
+                for (int i = _settings.StartYear; i <= _settings.EndYear; i++)
                 {
                     string year = i.ToString();
                     _boxScores.Add(year, new Dictionary<string, IMongoCollection<Box>>
